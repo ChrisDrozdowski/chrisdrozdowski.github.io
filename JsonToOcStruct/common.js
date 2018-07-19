90be22703c5e9e1c991a72c839cabe91b968153b
ChrisDrozdowski <drozdowski.chris@gmail.com>
2018-07-19 12:02:16 -0400

    Initial commit of JsonToOcStruct tool

---
+++ JsonToOcStruct/common.js
@@ -0,0 +1,142 @@
+$(function()
+{
+	var emptyInputMsg = "Paste JSON here.";
+	var emptyOutputMsg = "Struct will appear here.";
+	var formattedEmptyInputMsg = '<span style="color: #777;">'+emptyInputMsg+'</span>';
+	var formattedEmptyOutputMsg = '<span style="color: #777;">'+emptyOutputMsg+'</span>';
+	var verbose = $( "#verbose" ).prop( "checked");
+
+	$( "#verbose" ).change(function() {
+		verbose = $(this).prop( "checked");
+		if ($("#input").text() !== emptyInputMsg)
+		{
+			$('#input').keyup();
+		}
+	});
+
+	// Hides placeholder text.
+	$('#input').on('focus', function()
+	{
+		var val = $(this).text();
+		if (!val)
+		{
+			$(this).html(formattedEmptyInputMsg);
+			$('#output').html(formattedEmptyOutputMsg);
+		}
+		else if (val == emptyInputMsg)
+			$(this).html("");
+	});
+
+	// Shows placeholder text.
+	$('#input').on('blur', function()
+	{
+		var val = $(this).text();
+		if (!val)
+		{
+			$(this).html(formattedEmptyInputMsg);
+			$('#output').html(formattedEmptyOutputMsg);
+		}
+	}).blur();
+
+	// Automatically do the conversion.
+	$('#input').keyup(function()
+	{
+		var input = $(this).text();
+		if (!input)
+		{
+			$('#output').html(formattedEmptyOutputMsg);
+			return;
+		}
+
+		var output = jsonToOcStruct(input, verbose);
+
+		if (output.error) {
+			$('#output').html('<span class="clr-red">'+output.error+'</span>');
+			var parsedError = output.error.match(/Unexpected token .+ in JSON at position (\d+)/);
+			if(parsedError) {
+				try { 
+					var faultyIndex = parsedError.length == 2 && parsedError[1] && parseInt(parsedError[1]);
+					faultyIndex && $('#output').html(constructJSONErrorHTML(output.error, faultyIndex, input));
+				} catch(e) {}
+			}
+		}
+		else
+		{
+			var finalOutput = output.oc;
+			var coloredOutput = hljs.highlight("cpp", finalOutput);
+			$('#output').html(coloredOutput.value);
+		}
+	});
+
+	// Fill in sample JSON if the user wants to see an example.
+	$('#sample1').click(function()
+	{
+		$('#input').text(stringify(sampleJson1)).keyup();
+	});
+	$('#sample2').click(function()
+	{
+		$('#input').text(stringify(sampleJson2)).keyup();
+	});
+
+});
+
+function constructJSONErrorHTML(rawErrorMessage, errorIndex, json) {
+	var errorHeading = '<p><span class="clr-red">'+ rawErrorMessage +'</span><p>';
+	var markedPart = '<span class="json-go-faulty-char">' + json[errorIndex] + '</span>';
+	var markedJsonString = [json.slice(0, errorIndex), markedPart, json.slice(errorIndex+1)].join('');
+	var jsonStringLines = markedJsonString.split(/\n/);
+	for(var i = 0; i < jsonStringLines.length; i++) {
+
+		if(jsonStringLines[i].indexOf('<span class="json-go-faulty-char">') > -1)  // faulty line
+			var wrappedLine = '<div class="faulty-line">' + jsonStringLines[i] + '</div>';
+		else 
+			var wrappedLine = '<div>' + jsonStringLines[i] + '</div>';
+
+		jsonStringLines[i] = wrappedLine;
+	}
+	return (errorHeading + jsonStringLines.join(''));
+}
+
+// Stringifies JSON in the preferred manner.
+function stringify(json)
+{
+	return JSON.stringify(json, null, "\t");
+}
+
+// From the PUG REST API.
+var sampleJson1 = {
+  "PropertyTable": {
+    "Properties": [
+      {
+        "CID": 1,
+        "MolecularFormula": "C9H17NO4",
+        "MolecularWeight": 203.238,
+        "InChIKey": "RDHQFKQIGNGIED-UHFFFAOYSA-N"
+      },
+      {
+        "CID": 2,
+        "MolecularFormula": "C9H18NO4+",
+        "MolecularWeight": 204.246,
+        "InChIKey": "RDHQFKQIGNGIED-UHFFFAOYSA-O"
+      },
+      {
+        "CID": 3,
+        "MolecularFormula": "C7H8O4",
+        "MolecularWeight": 156.137,
+        "InChIKey": "INCSWYKICIYAHB-UHFFFAOYSA-N"
+      },
+      {
+        "CID": 4,
+        "MolecularFormula": "C3H9NO",
+        "MolecularWeight": 75.111,
+        "InChIKey": "HXKKHQJGJAFBHI-UHFFFAOYSA-N"
+      },
+      {
+        "CID": 5,
+        "MolecularFormula": "C3H8NO5P",
+        "MolecularWeight": 169.073,
+        "InChIKey": "HIQNVODXENYOFK-UHFFFAOYSA-N"
+      }
+    ]
+  }
+};
---
+++ JsonToOcStruct/json-to-oc-struct.js
@@ -0,0 +1,257 @@
+/*! jsonToOcStruct 1.0.0
+ *
+ * Created by Chris Drozdowski (drozdowski.chris@gmail.com)
+ *
+ * Licensed under MIT. License info available in project repository
+ * at https://github.com/chrisdrozdowski.
+ *
+ * Based on MIT-licensed project by Matt Holt (https://mholt.github.io/json-to-go/)
+ */
+
+function jsonToOcStruct(json, verbose)
+{
+    var oc = '',
+        data,
+        scope,
+        structs = [],
+        arrays = false,
+        verbose = (verbose || false),
+        // List of most keywords that cannot be used for identifiers. May not be complete.
+        keywords = ['array','auto','bool','break','case','char','const','continue','default','do','double',
+            'else','enum','extern','float','for','goto','if','int','long','register','return','short',
+            'signed','sizeof','static','string','struct','switch','typedef','uint','union','unsigned',
+            'vector','void','volatile','wchar_t','while'];
+
+        try {
+            if ('object' === typeof json) {
+                data = json;
+            }
+            else {
+                data = JSON.parse(json.replace(/\.0/g, '.1')); // Hack that forces floats to stay as floats.
+            }
+
+            scope = data;
+            parseScope(scope);
+
+            if (structs.length > 0) {
+                oc = '';
+
+                if (verbose && arrays) {
+                    oc += '// Be sure to add this include because of Array member(s).\n#include <Array.h>\n\n';
+                }
+
+                if (verbose) {
+                    oc += '// You can rename the structs but be thoughtful about it.\n\n';
+                }
+
+                for (var i = structs.length - 1; i >= 0; i--)
+                {
+                    if (verbose && 0 === i)
+                    {
+                        oc += '// This is the main struct to pass to JSON.ReadString in Origin C.\n'
+                    }
+                    oc += structs[i] + '\n';
+                }
+            }
+        }
+        catch (e) {
+            return {
+                oc: '',
+                error: e.message
+            };
+        }
+
+    return {oc: oc};
+
+    function parseScope(scope) {
+        if ('object' === typeof scope && null !== scope) {
+            if (Array.isArray(scope)) { // Array.
+                return parseArray(scope);
+            }
+            else { // JSON object.
+                return parseStruct(scope);
+             }
+        }
+        else { // Scalar.
+            return ocType(scope);
+        }
+     }
+
+    function parseArray(scope) {
+        var scalarType;
+
+        for (var i = 0; i < scope.length; i++) {
+            var prevType,
+                currType;
+
+            currType = ocType(scope[i]);
+
+            if ('struct' === currType) {
+                break;
+            }
+            // JSON doesn't support arrays of arrays only array of objects.
+            //else if ('array' === currType) {
+                //break;
+            //}
+            else if ('invalid' === currType) {
+                break;
+            }
+
+            if ('string' === currType) {
+                scalarType = 'string';
+                break;
+            }
+
+            if ('double' === currType) {
+                scalarType = 'double';
+                break;
+            }
+
+            // Prevent from reverting from an __int64 back to an int.
+            if ('__int64' === currType) {
+                scalarType = '__int64';
+            }
+
+            if ('__int64' !== scalarType) {
+                scalarType = currType;
+            }
+        }
+
+        if (scalarType) {
+            return 'vector<' + scalarType + '>';
+        }
+
+        if ('struct' === currType && scope.length > 0) {
+            arrays = true;
+            var varType = parseStruct(scope[0]);
+            return 'Array<' + varType + '&>';
+        }
+
+        // JSON doesn't support arrays of arrays only array of objects.
+        //if ('array' === currType && scope.length > 0) {
+        //}
+
+    }
+
+    function parseStruct(scope) {
+        var struct = '',
+            current = -1,
+            constructors = '';
+
+        structs.push(struct);
+        current = structs.length - 1;
+
+        if (0 === current) {
+            structs[current] = 'struct stMain {\n';
+        }
+        else
+        {
+            base = 'Dep';
+            structs[current] = 'struct stDep' + current + ' {\n';
+        }
+
+
+        var varNames = Object.keys(scope);
+        for (var i in varNames) {
+            var varName = varNames[i];
+            var varType = parseScope(scope[varName]);
+
+            structs[current] += '\t' + varType + ' ';
+            structs[current] += ' ' + varName + ';';
+
+            if (verbose && !validName(varName)) {
+                structs[current] += '\t// Member name is likely invalid.';
+            }
+
+            if (verbose && varType.indexOf('__int64') > -1) {
+                structs[current] += '\t// May be double.';
+            }
+            else if (verbose && varType.indexOf('int') > -1) {
+                structs[current] += '\t// May be __int64 or double.';
+            }
+
+            structs[current] += '\n';
+
+            if ('Array' === varType.substring(0, 5)) {
+                constructors += '\n\t\t' + varName + '.SetAsOwner(true);\n';
+            }
+        }
+
+        if (constructors.length > 0)
+        {
+            structs[current] += '\tst' + current + '()\n\t{' + constructors + '\t}\n';
+        }
+
+        structs[current] += '}\n';
+
+        if (0 === current) {
+            return;
+        }
+
+        return 'stDep' + current;
+    }
+
+    function ocType(val) {
+        if (null === val) {
+            return 'invalid';
+        }
+
+        switch (typeof val) {
+            case 'string':
+                    return 'string';
+
+            case 'number':
+                if (0 === (val % 1)) {
+                    if (val > -2147483648 && val < 2147483647) {
+                        return 'int';
+                    }
+                    else {
+                        return '__int64';
+                    }
+                }
+                else {
+                    return 'double';
+                }
+
+            case 'boolean':
+                return 'bool';
+
+            case 'object':
+                if (Array.isArray(val)) {
+                    return 'array';
+                }
+                else {
+                    return 'struct';
+                }
+
+            default:
+                return 'invalid';
+        }
+    }
+    
+    function validName(str) {
+        if (false === /^[a-zA-Z_][a-zA-Z0-9_]{0,31}$/.test(str)) {
+            return false;
+        }
+        else if (keywords.indexOf(str) > -1) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+}
+
+if (typeof module != 'undefined')
+{
+    if (!module.parent) {
+        process.stdin.on('data', function(buf)
+        {
+            var json = buf.toString('utf8');
+            console.log(jsonToOcStruct(json).oc);
+        })
+    }
+    else {
+        module.exports = jsonToOcStruct;
+    }
+}
